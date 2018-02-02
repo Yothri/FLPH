09e17576d572a294dfe65b5ff45154fc68f5eb7f
johmarjac <johannesmjacobs@gmail.com>
2018-02-02 23:30:11 +0100

    NetStandard2.0 Support!

--- FLPH/Network/Client/PhotonProxyClient.cs
+++ FLPH/Network/Client/PhotonProxyClient.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Linq;
 using System.Net.Sockets;
-using AdvancedConsole;
 using Ether.Network.Packets;
 using Ether.Network.Photon.Client;
-using Ether.Network.Photon.Common;
-using FLPH.Util;
 using AppContext = FLPH.Core.AppContext;
 
 namespace FLPH.Network.Client
@@ -25,20 +21,6 @@ namespace FLPH.Network.Client
         public override void HandleMessage(INetPacketStream packet)
         {
             base.HandleMessage(packet);
-
-            var data = packet.Read<byte>(packet.Size);
-            Console.WriteLine($"Client -> Server | Size: [{data.Length}]");
-            AConsole.WriteHexView(data);
-            Console.WriteLine();
-            var hookData = data.Skip(1).ToArray();
-            if(data[0] != 240)
-                hookData = AppContext.Instance.HookManager.CallGameServerToClientHooks(data.Skip(8).ToArray());
-
-            using (var response = new PhotonPacket(data[0] == 240))
-            {
-                response.Write(hookData, 0, hookData.Length);
-                AppContext.Instance.ProxyServer.SendTo(AppContext.Instance.ProxyServer.Clients, response);
-            }
         }
 
         protected override void OnDisconnected()
--- FLPH/Network/Common/PhotonProxyConnection.cs
+++ FLPH/Network/Common/PhotonProxyConnection.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Linq;
-using AdvancedConsole;
-using Ether.Network.Packets;
+﻿using Ether.Network.Packets;
 using Ether.Network.Photon.Common;
-using AppContext = FLPH.Core.AppContext;
 
 namespace FLPH.Network.Common
 {
@@ -12,20 +8,6 @@ namespace FLPH.Network.Common
         public override void HandleMessage(INetPacketStream packet)
         {
             base.HandleMessage(packet);
-
-            var data = packet.Read<byte>(packet.Size);
-            Console.WriteLine($"Server -> Client | Size: [{data.Length}]");
-            AConsole.WriteHexView(data);
-            Console.WriteLine();
-            var hookData = data.Skip(1).ToArray();
-            if (data[0] != 240)
-                hookData = AppContext.Instance.HookManager.CallClientToGameServerHooks(data.Skip(8).ToArray());
-
-            using (var response = new PhotonPacket(data[0] == 240))
-            {
-                response.Write(hookData, 0, hookData.Length);
-                AppContext.Instance.ProxyClient.Send(response);
-            }
         }
     }
 }
